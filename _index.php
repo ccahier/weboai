fbbf81bb9fe197a0fab53f554aa45e60632fb36b
glorieux <glorieux@81795817-665f-4444-aa81-1a1113da5fb2>
2014-11-25 09:38:21 +0000

    Changement de pied pour l’installation

---
+++ _index.php
@@ -0,0 +1,38 @@
+<?php
+include(dirname(__FILE__).'/Conf.php'); // importer la configuration
+include(dirname(__FILE__).'/lib/Pub.php');
+$pub=new Pub(Conf::$sqlite);
+
+
+
+?><!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>Weboai</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo $pub->homehref . Conf::$weboaihref ?>lib/weboai.css" />
+  </head>
+  <body>
+    <main>
+      <?php 
+if ($pub->path == '') {
+  $pub->sets();
+}
+else if (strpos($pub->path, 'set/') === 0) {
+  $setspec = trim(substr($pub->path, 4), '/');
+  $pub->set($setspec);
+}
+else if (strpos($pub->path, 'record/') === 0) {
+  $oai_identifier = substr($pub->path, strpos($pub->path, '/') + 1);
+  $pub->record($oai_identifier);
+}
+else {
+  echo '<p>La page ' . $pub->path . ' n’a pas été trouvée</p>';
+}
+// $pub->chrono();
+// $pub->biblio(); 
+              ?>
+    </main>
+    <script type="text/javascript" src="<?php echo $pub->homehref . Conf::$weboaihref ?>lib/Sortable.js">//</script>
+  </body>
+</html>
