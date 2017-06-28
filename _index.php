daae5aa30f617860d831da84d8bd5d5865cf9fa9
glorieux-f <frederic.glorieux@fictif.org>
2017-06-29 00:26:20 +0200

    Changement de contrat pour la conf

--- _index.php
+++ _index.php
@@ -1,22 +1,22 @@
 <?php
-include(dirname(__FILE__).'/Conf.php'); // importer la configuration
+$conf = include( 'Conf.php' ); // importer la configuration
 include(dirname(__FILE__).'/lib/Pub.php');
 // si fichier .htaccess alors on suppose que les URI seront de type /set/setSpec /record/oai_identifier
-if ( file_exists('.htaccess') ) $pub=new Pub( Conf::$sqlite, "PATHINFO" );
+if ( file_exists('.htaccess') ) $pub=new Pub( $conf['sqlite'], "PATHINFO" );
 // sinon ?set=setSpec, ?record=oai_identifier
-else $pub=new Pub( Conf::$sqlite );
+else $pub=new Pub( $conf['sqlite'] );
 
 
 ?><!DOCTYPE html>
 <html>
   <head>
     <meta charset="UTF-8" />
-    <title><?php echo Conf::$repositoryName ?></title>
-    <link rel="stylesheet" type="text/css" href="<?php echo $pub->homehref . Conf::$weboaihref ?>lib/weboai.css" />
+    <title><?php echo $conf['repositoryName'] ?></title>
+    <link rel="stylesheet" type="text/css" href="<?php echo $pub->homehref . $conf['weboaihref'] ?>lib/weboai.css" />
   </head>
   <body>
     <main>
-      <h1><?php echo Conf::$repositoryName ?></h1>
+      <h1><?php echo $conf['repositoryName'] ?></h1>
       <?php
 // un set demandé par paramètre (si pas de RewriteRule dans le .htaccess)
 if ( isset($_REQUEST['set']) ) {
@@ -45,6 +45,6 @@ else {
 }
               ?>
     </main>
-    <script type="text/javascript" src="<?php echo $pub->homehref . Conf::$weboaihref ?>lib/Sortable.js">//</script>
+    <script type="text/javascript" src="<?php echo $pub->homehref . $conf['weboaihref'] ?>lib/Sortable.js">//</script>
   </body>
 </html>
