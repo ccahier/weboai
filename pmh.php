678679ca850516f9cac6ebcc93daedb4c576d496
glorieux-f <frederic.glorieux@fictif.org>
2017-06-29 11:05:50 +0200

    Lien à la conf

--- pmh.php
+++ pmh.php
@@ -1,5 +1,5 @@
 <?php
-$conf = include (dirname(__FILE__).'/Conf.php'); // importer la configuration
+$conf = include (dirname(__FILE__).'/conf.php'); // importer la configuration
 include (dirname(__FILE__).'/lib/Pmh.php'); // importer le le serveur OAI
 new Pmh( $conf['sqlite'] );
 
